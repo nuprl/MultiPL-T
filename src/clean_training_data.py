09dbd3fd8e1b5f62982cb7d716d652505ab3bcd1
cassanof <federico.cassano@federico.codes>
2023-07-27 16:47:11 -0700

    cleanup

--- src/clean_training_data.py
+++ src/clean_training_data.py
@@ -1,22 +1,22 @@
-import datasets 
+import datasets
 import argparse
 import re
 
-def clean_racket(ex):
-    content_lines = ex["content"].split("\n")
+
+def clean_racket(sol):
+    content_lines = sol.split("\n")
     # regex to recognize linestart ;; #
     canon_regex = re.compile(r"^\s*;;\s*#.*$")
     comment_line = re.compile(r"^\s*;;\s*$")
     whitespace = re.compile(r"^\s*$")
     new_content = []
     for line in content_lines:
-        if not re.match(canon_regex, line) and re.match(comment_line, line) and re.match(whitespace, line): 
+        if not re.match(canon_regex, line) and re.match(comment_line, line) and re.match(whitespace, line):
             new_content.append(line)
-    ex["content"] = "\n".join(new_content)
-    return ex
+    return "\n".join(new_content)
 
-def clean_lua(ex):
-    sol = ex["content"]
+
+def clean_lua(sol):
     sol = sol[:sol.find("\nlu =")]
     sol_lines = sol.split("\n")
     # TODO: the "**" is the old format, remove this later
@@ -34,28 +34,35 @@ def clean_lua(ex):
     sol_lines = [line for line in sol_lines if line.rstrip() !=
                  "--" and line != ""]
 
-    sol = "\n".join(sol_lines)
-    ex["content"] = sol
+    return "\n".join(sol_lines)
+
+
+def clean_lua_ex(ex):
+    ex["content"] = clean_lua(ex["content"])
     return ex
 
-    
 
+def clean_racket_ex(ex):
+    ex["content"] = clean_racket(ex["content"])
+    return ex
 
-    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input-dataset", type=str, required=True, help="Input dataset name")
-    parser.add_argument("--output-dataset", type=str, required=True, help="Output dataset name")
-    parser.add_argument("--language", type=str, required=True, help="Language of the dataset")
+    parser.add_argument("--input-dataset", type=str,
+                        required=True, help="Input dataset name")
+    parser.add_argument("--output-dataset", type=str,
+                        required=True, help="Output dataset name")
+    parser.add_argument("--language", type=str, required=True,
+                        help="Language of the dataset")
     parser.add_argument("--push", action="store_true", help="Push to hub")
     args = parser.parse_args()
-    
+
     dataset = datasets.load_dataset(args.input_dataset)
     if args.language == "racket":
-        dataset = dataset.map(clean_racket)
+        dataset = dataset.map(clean_racket_ex)
     elif args.language == "lua":
-        dataset = dataset.map(clean_lua)
+        dataset = dataset.map(clean_lua_ex)
     else:
         # crash with unimplemented language
         raise NotImplementedError(f"Language {args.language} not implemented")
