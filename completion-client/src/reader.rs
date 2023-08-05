cca7bfd641d3080f082b03dc81e883396e5d639e
GouwarPower <jgouwar@gmail.com>
2023-08-05 15:51:06 -0400

    break

--- completion-client/src/reader.rs
+++ completion-client/src/reader.rs
@@ -23,16 +23,20 @@ pub async fn read_input_jsonl(
     let f = tokio::io::BufReader::new(f);
     let mut lines = f.lines();
     while let Some(()) = read_toks.recv().await {
-        if let Some(line) = lines.next_line().await.unwrap() { 
-            let prompt: Prompt = serde_json::from_str(&line).expect("Should be valid prompt");
-            let id = get_id_from_path(prompt.original.to_string());
-            if !seen_ids.contains(&id) {
-                let prog = Program::from(prompt);
-                let _ = compl_queue.send(Box::new(prog)).await;
-            } else {
-                let _ = log_queue.send((format!("Ignoring prompt: {}", id), None)).await;
+        loop {
+            if let Some(line) = lines.next_line().await.unwrap() { 
+                let prompt: Prompt = serde_json::from_str(&line).expect("Should be valid prompt");
+                let id = get_id_from_path(prompt.original.to_string());
+                if !seen_ids.contains(&id) {
+                    let prog = Program::from(prompt);
+                    let _ = compl_queue.send(Box::new(prog)).await;
+                    break;
+                } else {
+                    let _ = log_queue.send((format!("Ignoring prompt: {}", id), None)).await;
+                }
             }
         }
+        
     }
     let _ = log_queue.send((String::from("Read all prompts"), None)).await;
 }
