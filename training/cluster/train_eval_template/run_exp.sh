e5b0ab85052c7c8e5f5ea3915bab100a4cb017c4
GouwarPower <jgouwar@gmail.com>
2023-08-10 12:32:34 -0400

    x

--- training/cluster/train_eval_template/run_exp.sh
+++ training/cluster/train_eval_template/run_exp.sh
@@ -2,7 +2,7 @@ PASS_K=/home/johngouwar/repos/MultiPL-E/pass_k.py
 
 CUDA_VISIBLE_DEVICES=0 python3 train.py 
 
-for $check in ./checkpoint*; do
+for check in ./checkpoint*; do
     # Run the evaluation
     check_name=$(basename $check)
     # Find directory in check with prefix eval
