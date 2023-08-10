cca8018ea35ecf041422f311543684127f7b8d04
GouwarPower <jgouwar@gmail.com>
2023-08-10 13:17:54 -0400

    fix

--- training/cluster/train_eval_template/run_exp.sh
+++ training/cluster/train_eval_template/run_exp.sh
@@ -3,9 +3,7 @@ PASS_K=/home/johngouwar/repos/MultiPL-E/pass_k.py
 CUDA_VISIBLE_DEVICES=0 python3 train.py 
 
 for check in ./checkpoint*; do
-    # Run the evaluation
     check_name=$(basename $check)
-    # Find directory in check with prefix eval
     eval_dir=$(find $check -type d -name "eval*")
     mkdir -p tmp_home 
     podman run \
@@ -13,5 +11,5 @@ for check in ./checkpoint*; do
       --volume $eval_dir:/dataset:rw \
       multipl-e-evaluation:latest \
       --dir /dataset --output-dir /dataset --recursive
-    python3 $PASS_K $check/eval | tee passk_$check_name.csv
+    python3 $PASS_K $eval_dir | tee passk_$check_name.csv
 done
