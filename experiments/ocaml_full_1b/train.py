9db7049b69c8a7dc540ec8d9bdc7dda5dc356800
cassanof <federico.cassano@federico.codes>
2023-08-14 09:39:25 -0700

    ocaml exps

---
+++ experiments/ocaml_full_1b/train.py
@@ -0,0 +1,18 @@
+# This script was run on Boa
+from code_llms.training import simple_train
+from pathlib import Path
+import datasets
+
+train_data = datasets.load_dataset("nuprl/multipl-t_ocaml_paper_aug", split="train")
+
+simple_train(
+    model="/home/arjun/models/starcoderbase-1b",
+    batch_size=8,
+    epochs=7,
+    learning_rate=3e-5,
+    warmup_steps=10,
+    train_data=train_data,
+    test_data="../../datasets/humaneval-ml.jsonl",
+    log_dir="log",
+)
+
---
+++ experiments/ocaml_subset_1b/train.py
@@ -0,0 +1,19 @@
+# This script was run on Boa
+from code_llms.training import simple_train
+from pathlib import Path
+import datasets
+
+train_data = datasets.load_dataset("nuprl/multipl-t_ocaml_paper_aug", split="train")
+
+simple_train(
+    model="/home/arjun/models/starcoderbase-1b",
+    batch_size=8,
+    epochs=7,
+    learning_rate=3e-5,
+    warmup_steps=10,
+    train_data=train_data,
+    test_data="../../datasets/humaneval-ml.jsonl",
+    dataset_limit=25000,
+    log_dir="log",
+)
+
