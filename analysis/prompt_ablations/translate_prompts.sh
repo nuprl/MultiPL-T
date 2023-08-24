89365b10f53b26b11d88908ac79da54a361f0f9c
cassanof <federico.cassano@federico.codes>
2023-08-24 10:43:58 -0700

    dirs

--- analysis/prompt_ablations/translate_prompts.sh
+++ analysis/prompt_ablations/translate_prompts.sh
@@ -8,7 +8,7 @@ pushd ../../MultiPL-E/dataset_builder
 
 for lang in $LANGS; do
     echo "Translating $lang"
-    LANGDIR=../../analysis/prompt_ablations/canonical_comment_prompts/$lang
+    LANGDIR=../../analysis/prompt_ablations/canonical_comment_prompts/canonical_$lang
     PROMPTPATH=../../analysis/prompt_ablations/canonical_comment_prompts/$lang-prompts.jsonl
     python3 prepare_prompts_json.py \
       --lang humaneval_to_$lang.py\
@@ -25,7 +25,7 @@ for lang in $LANGS; do
       --lang humaneval_to_$lang.py\
       --output ../../analysis/prompt_ablations/base_prompts/$lang-prompts.jsonl \
       --originals ../../multipl_e_target_adaptor/stack-clean-python/
-    mkdir -p ../../analysis/prompt_ablations/base_prompts/$lang
+    mkdir -p ../../analysis/prompt_ablations/base_prompts/base_$lang
 done
 
 
