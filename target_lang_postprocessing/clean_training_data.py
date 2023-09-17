30680e6b9051c9976aba5745ec4a88ffecfe93e4
elleven11 <federico.cassano@federico.codes>
2023-09-16 23:26:21 -0400

    add luau, it's a bit different from lua

--- target_lang_postprocessing/clean_training_data.py
+++ target_lang_postprocessing/clean_training_data.py
@@ -43,6 +43,27 @@ def clean_lua(sol):
     return "\n".join(sol_lines)
 
 
+def clean_luau(sol):
+    sol = sol[:sol.find("\nlocal function _half_equals")]
+    sol_lines = sol.split("\n")
+    if "## Canonical Python Solution ##" in sol_lines[0]:
+        # remove the canonical solution from comment
+        # canonical solution lines start with " * "
+        not_canonical_i = 0
+        for i, line in enumerate(sol_lines[1:]):
+            r_i = i + 1
+            if not line.startswith("-- *") and not line.startswith("-- #"):
+                not_canonical_i = r_i
+                break
+        sol_lines = sol_lines[not_canonical_i:]
+
+    # remove every line that is empty
+    sol_lines = [line for line in sol_lines if line.rstrip() !=
+                 "--" and line != ""]
+
+    return "\n".join(sol_lines)
+
+
 def clean_ml(sol):
     sol = sol[:sol.find("\nlet assertions")]
     sol_lines = sol.split("\n")
@@ -58,6 +79,7 @@ def clean_ml(sol):
     sol_lines = [line for line in sol_lines if line.rstrip() != ""]
     return "\n".join(sol_lines)
 
+
 def clean_julia(sol):
     sol_lines = sol.split("\n")
     if "## Canonical Python Solution ##" in sol_lines[0]:
@@ -69,12 +91,15 @@ def clean_julia(sol):
         sol_lines = sol_lines[not_canonical_i:]
         sol_lines = ['"""'] + sol_lines
     sol_lines = [line for line in sol_lines if line.rstrip() != ""]
-    return "\n".join(sol_lines) + "\nend" # since end is the stop token
+    return "\n".join(sol_lines) + "\nend"  # since end is the stop token
+
+
 def clean_ex(ex, cleaner):
     ex["content"] = cleaner(ex["content"])
     return ex
 
 
+def clean_luau_ex(ex): return clean_ex(ex, clean_luau)
 def clean_lua_ex(ex): return clean_ex(ex, clean_lua)
 def clean_racket_ex(ex): return clean_ex(ex, clean_racket)
 def clean_ml_ex(ex): return clean_ex(ex, clean_ml)
@@ -92,7 +117,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.input_dataset.endswith(".jsonl"):
-        dataset = datasets.load_dataset("json", data_files=args.input_dataset, split="train")
+        dataset = datasets.load_dataset(
+            "json", data_files=args.input_dataset, split="train")
     else:
         dataset = datasets.load_dataset(args.input_dataset, split="train")
     cleaner = None
--- target_lang_postprocessing/utils.py
+++ target_lang_postprocessing/utils.py
@@ -1,8 +1,10 @@
-from clean_training_data import clean_lua, clean_racket, clean_ml
+from clean_training_data import clean_lua, clean_luau, clean_racket, clean_ml
 
 def clean_sol_prompt(lang, sol):
     if lang == "lua":
         return clean_lua(sol)
+    if lang == "luau":
+        return clean_luau(sol)
     elif lang == "racket":
         return clean_racket(sol)
     elif lang == "ml":
