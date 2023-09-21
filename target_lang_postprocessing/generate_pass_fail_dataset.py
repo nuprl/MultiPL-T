74434385d1a5769bb0718d8aea71227bb2fa8593
cassanof <federico.cassano@federico.codes>
2023-09-21 14:05:20 -0400

    a

--- target_lang_postprocessing/generate_pass_fail_dataset.py
+++ target_lang_postprocessing/generate_pass_fail_dataset.py
@@ -15,6 +15,7 @@ args = pa.parse_args()
 
 funcs_pass = []
 funcs_fail = []
+pass_rates = []
 tests = []
 num_tests = []
 ids = []
@@ -49,13 +50,21 @@ for path in progressbar(make_path_iterator(), max_value=len(list(make_path_itera
 
     num_tests.append(_num_tests)
     tests.append(tests_code)
+    pass_rates.append(len(passed) / len(results))
     funcs_pass.append(passed)
     funcs_fail.append(failed)
     ids.append(e_id)
 
 new_ds = datasets.Dataset.from_dict(
-    {"id": ids, "num_tests": num_tests, "tests": tests,
-        "pass": funcs_pass, "fail": funcs_fail})
+    {
+        "id": ids,
+        "num_tests": num_tests,
+        "tests": tests,
+        "pass_rate": pass_rates,
+        "pass": funcs_pass,
+        "fail": funcs_fail
+    }
+)
 new_ds = new_ds.filter(lambda x: x["num_tests"] >= args.min_tests)
 new_ds = new_ds.filter(lambda x: len(x["pass"]) > 0 and len(x["fail"]) > 0)
 print(len(new_ds))
