98f1ce2101fae3960359e946823df8a4642696c6
cassanof <federico.cassano@federico.codes>
2023-10-17 16:32:27 -0400

    tokenizer push

--- experiments/push_checkpoints.py
+++ experiments/push_checkpoints.py
@@ -1,10 +1,12 @@
-from transformers import AutoModelForCausalLM
+from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 import pathlib
 import argparse
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("--dir", type=str, required=True)
+arg_parser.add_argument("--tokenizer", type=str,
+                        default="", help="Optional, tokenizer to push to hub")
 arg_parser.add_argument("--base_repo", type=str,
                         default="nuprl/MultiPLCoder-1b")
 arg_parser.add_argument("--checkpoint_sep", type=str,
@@ -22,6 +24,10 @@ for path in pathlib.Path(args.dir).rglob(f"checkpoint{args.checkpoint_sep}*"):
     checkpoints.append(path.name)
 
 dir_name = pathlib.Path(args.dir).name
+
+if args.tokenizer:
+    tok = AutoTokenizer.from_pretrained(args.tokenizer)
+    tok.push_to_hub(args.base_repo, private=True)
 
 checkpoints.sort(key=lambda x: int(x.split(args.checkpoint_sep)[1]))
 for epoch, checkpoint in enumerate(checkpoints):
