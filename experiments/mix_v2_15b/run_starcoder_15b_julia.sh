484e30ae6607cbaff610febfde95e14342933ad5
cassanof <federico.cassano@federico.codes>
2024-02-01 17:54:58 -0500

    mix v2 cfg

---
+++ experiments/mix_v2_15b/run_starcoder_15b_julia.sh
@@ -0,0 +1,27 @@
+# need to give deepspeed config file as argument
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied. Please give deepspeed config file as argument"
+    exit 1
+fi
+python3 -m torch.distributed.launch \
+        --nproc_per_node 8 \
+        main.py \
+        --deepspeed="$1" \
+        --model_path="bigcode/starcoderbase" \
+        --dataset_name="nuprl-staging/multipl-t-mixed-deduped" \
+        --no_approx_tokens \
+        --output_dir="./model_starcoder15b_mix_v2" \
+        --seq_length 2048 \
+        --epochs 6 \
+        --batch_size 1 \
+        --gradient_accumulation_steps 4 \
+        --learning_rate 2e-5 \
+        --fa2 \
+        --num_warmup_steps 10 \
+        --num_workers=$(expr $(nproc --all) - 4) \
+        --no_fp16 \
+        --bf16 \
+        --eval_freq 0 \
+        --perc_valid_set 0.0 \
+        --save_total_limit 20
